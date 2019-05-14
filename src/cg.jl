76efede5622496b81d5fb98d2a7c2aa1466824d8
Stefano Mandelli <linuxfree2@gmail.com>
2019-05-14 14:46:49 +0200

    Weird behaviour ...

--- src/cg.jl
+++ src/cg.jl
@@ -21,10 +21,10 @@ function cg!(A, b::Array; tol::Float64=1e-6, maxIter::Int64=100,
     genblas_scal!(-one(Float64), data.r)
     genblas_axpy!(one(Float64), b, data.r)
 
-    residual_0 = genblas_nrm2(data.r)
-    if residual_0 <= tol
-        return 2, 0
-    end
+    # residual_0 = genblas_nrm2(data.r)
+    # if residual_0 <= tol
+    #     return 2, 0
+    # end
 
     for iter = 1:maxIter
         data.Ap .= A(data.p)
