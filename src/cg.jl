64bb0669465377739f1525ce87224d5981ce4cf1
Stefano Mandelli <linuxfree2@gmail.com>
2019-06-21 17:59:50 +0200

    Fixed a lots of bugs inside the cg.jl code
    There was a lots of bugs, many of them due to typos, that now
    are fixed. I have checked the whole code and, actually,
    there was a bug in the precondictioner application and in the
    Ap matrix building routine.
    
    I have test the ConjGrad package with a strip2-like tod
    and it's work fine.

--- src/cg.jl
+++ src/cg.jl
@@ -7,24 +7,31 @@ struct CGData{T<:Real}
                                             zeros(T, n), zeros(T, n))
 end
 
-function cg!(A, b::Array; tol::Float64=1e-6, maxIter::Int64=100,
-             precon=copy!, data=CGData{Float64}(length(b)))
+function cg!(A, b::Array, x::Array; tol::Float64=1e-6,
+             maxIter::Int64=10, precon=copy!, data=CGData{Float64}(length(b)))
 
     n = length(b)
     n_iter = 0
 
-    x = zeros(n)
-    data.r .= b
-    precon(data.z, data.r)
-    data.p .= data.z # ok
+    if genblas_nrm2(b) == 0.0
+        x .= 0.0
+        return 1, 0
+    end
 
-    genblas_scal!(-one(Float64), data.r)
-    genblas_axpy!(one(Float64), b, data.r)
+    x .= A(data.r)
 
+    genblas_scal!(-1.0, data.r)
+    genblas_axpy!(1.0, b, data.r)
     residual_0 = genblas_nrm2(data.r)
-    # if residual_0 <= tol
-    #     return 2, 0
-    # end
+
+    println(residual_0) # per ora si spacca qua!
+
+    if residual_0 <= tol
+        return 2, 0
+    end
+
+    precon(data.z, data.r)
+    @. data.p = data.z
 
     for iter = 1:maxIter
         data.Ap .= A(data.p)
@@ -33,13 +40,16 @@ function cg!(A, b::Array; tol::Float64=1e-6, maxIter::Int64=100,
         if alpha == Inf || alpha < 0
             return -13, iter
         end
-        # x = x + alha * p
+
         genblas_axpy!(alpha, data.p, x)
         genblas_axpy!(-alpha, data.Ap, data.r)
         residual = genblas_nrm2(data.r)/residual_0
+        println(residual)
+
         if residual <= tol
             return 30, iter
         end
+
         precon(data.z, data.r)
         beta = genblas_dot(data.z, data.r)/gamma
         genblas_scal!(beta, data.p)
@@ -49,10 +59,10 @@ function cg!(A, b::Array; tol::Float64=1e-6, maxIter::Int64=100,
 end
 
 function cg(A, b::Array;
-            tol::Float64=1e-6, maxIter::Int64=100,
+            tol::Float64=1e-6, maxIter::Int64=10,
             precon=copy!, data=CGData{Float64}(length(b)))
     x = zeros(length(b))
-    exit_code, num_iters = cg!(A, b, tol=tol, maxIter=maxIter,
+    exit_code, num_iters = cg!(A, b, x, tol=tol, maxIter=maxIter,
                                precon=precon, data=data)
     return x, exit_code, num_iters
 end
--- test/cg_test.jl
+++ test/cg_test.jl
@@ -1,7 +1,67 @@
-using ConjGrad
+include("../src/ConjGrad.jl")
+#using ConjGrad
 using LinearAlgebra, SparseArrays
 using Test
+using Random
+using FFTW
 
-# it's a mess!
 
-@test test_cg()
+using JLD
+@load("../strip2/ciao")
+
+N = 1024
+
+function binned_map(N_pix::Int, tod::Array{Float64, 1},
+                    points::Array{Int64, 2})
+    map = zeros(N_pix, N_pix)
+    hit = zeros(N_pix, N_pix)
+
+    for i = 1:length(tod)
+        map[points[i, 1], points[i, 2]] += tod[i]
+        hit[points[i, 1], points[i, 2]] += 1.0
+    end
+
+    return map, hit
+
+end
+
+
+function get_tod(sky::Array{Float64, 2}, points::Array{Int64, 2})
+
+    Num_of_pixels = size(points)[1]
+    tod = [sky[points[i,1], points[i,2]] for i = 1:Num_of_pixels]
+
+    return tod
+
+end
+
+# inserire il denoise
+function denoise(tod::Array{Float64, 1}, noise_s::Array{Float64, 1})
+  ftod = fft(tod)
+  ftod ./= noise_s
+  tod = real.(ifft(ftod))
+  return tod
+end
+
+b = zeros(N*N)
+for i in dataset_baselines
+  tod = denoise(i.time_order_data, i.noise)
+  a, h = binned_map(N, tod, i.pointing)
+  b .+= reshape(a, N*N, 1)[:, 1]
+end
+
+
+function A(x)
+  res = zeros(N, N)
+  for i in dataset_baselines
+    tod = get_tod(reshape(x, N, N), i.pointing)
+    tod = denoise(tod, i.noise)
+    mat, hmat = binned_map(N, tod, i.pointing)
+    res .+= mat
+  end
+  return reshape(res, N*N, 1)[:, 1]
+end
+
+x=zeros(length(b))
+data = ConjGrad.CGData{Float64}(length(b))
+x, exit_code, num_iters=ConjGrad.cg(A, b, tol=1e-3, precon=A)
